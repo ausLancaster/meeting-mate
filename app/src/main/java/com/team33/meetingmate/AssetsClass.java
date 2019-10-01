cb9a38987a3e4f03509f8194cd416826055297f1
Austin Lancaster <aus@lancaster@gmail.com>
2019-10-01 12:41:23 +1000

    Use AsyncTask to get events

---
+++ app/src/main/java/com/team33/meetingmate/AssetsClass.java
@@ -0,0 +1,10 @@
+package com.team33.meetingmate;
+
+import android.content.res.AssetManager;
+
+public class AssetsClass {
+    //More code here. Constructor is not necessary.
+    AssetManager getAssets() {
+        return getAssets();
+    }
+}
--- app/src/main/java/com/team33/meetingmate/CalendarActivity.java
+++ app/src/main/java/com/team33/meetingmate/CalendarActivity.java
@@ -1,15 +1,18 @@
 package com.team33.meetingmate;
 
+import android.accounts.Account;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
@@ -21,16 +24,13 @@ import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
-import android.view.View;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
@@ -38,13 +38,17 @@ public class CalendarActivity extends AppCompatActivity {
     private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final String PREF_ACCOUNT_NAME = "accountName";
 
     /**
      * Global instance of the scopes required by this quickstart.
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
     private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
-    private static final String CREDENTIALS_FILE_PATH = "android.resource://com.team33.meetingmate/credentials.json";
+    private static final String CREDENTIALS_FILE_PATH = "credentials.json";
+
+    private Events events;
+    private Calendar service;
 
     /**
      * Creates an authorized Credential object.
@@ -52,13 +56,17 @@ public class CalendarActivity extends AppCompatActivity {
      * @return An authorized Credential object.
      * @throws IOException If the credentials.json file cannot be found.
      */
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT, Context context) throws IOException {
         // Load client secrets.
-        InputStream in = CalendarActivity.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        AssetManager assetManager = context.getAssets();
+        InputStream in = assetManager.open(CREDENTIALS_FILE_PATH);;
         if (in == null) {
             throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
         }
+
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        Log.d("Calendar", "Done Credentials");
 
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
@@ -66,31 +74,66 @@ public class CalendarActivity extends AppCompatActivity {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
+        Log.d("Calendar", "NExt");
+
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        Log.d("Calendar", "NExt");
+
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        Log.d("Calendar", "Credentials succeeded");
+        return credential;
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.d("Calendar", "Create");
-
-        try {
             // Build a new authorized API client service.
+            /*final NetHttpTransport HTTP_TRANSPORT = new com.google.api.client.http.javanet.NetHttpTransport();
+            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT, getApplicationContext()))
+                    .setApplicationName(APPLICATION_NAME)
+                    .build();*/
+            GoogleAccountCredential credential =
+                    GoogleAccountCredential.usingOAuth2(this, SCOPES);
+            SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+            //credential.setSelectedAccountName(settings.getString(PREF_ACCOUNT_NAME, null));
+            credential.setSelectedAccount(new Account("aus.lancaster@gmail.com", "com.team33.meetingmate"));
+
             final NetHttpTransport HTTP_TRANSPORT = new com.google.api.client.http.javanet.NetHttpTransport();
-            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+            service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
                     .setApplicationName(APPLICATION_NAME)
                     .build();
 
+            Log.d("Calendar", "Execute");
+            new GetEvents().execute();
+
+    }
+
+    private class GetEvents extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+
             // List the next 10 events from the primary calendar.
             DateTime now = new DateTime(System.currentTimeMillis());
-            Events events = service.events().list("primary")
-                    .setMaxResults(10)
-                    .setTimeMin(now)
-                    .setOrderBy("startTime")
-                    .setSingleEvents(true)
-                    .execute();
+
+            try {
+                events = service.events().list("primary")
+                        .setMaxResults(10)
+                        .setTimeMin(now)
+                        .setOrderBy("startTime")
+                        .setSingleEvents(true)
+                        .execute();
+            } catch (IOException e) {
+                Log.d("Calendar", events != null ? events.toString() : "null");
+            }
+
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String token) {
             List<Event> items = events.getItems();
             if (items.isEmpty()) {
                 Log.d("Calendar", "No upcoming events found.");
@@ -104,9 +147,6 @@ public class CalendarActivity extends AppCompatActivity {
                     Log.d("Calendar", event.getSummary() + " " + start);
                 }
             }
-        } catch(IOException e) {
-            Log.d("Calendar", e.getMessage());
         }
     }
-
 }
