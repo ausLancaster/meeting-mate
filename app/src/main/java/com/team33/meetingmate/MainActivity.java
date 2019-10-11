01ed70cab8188d7a6167d3c68ad87cd8475683bd
Pallabi <pbhattachar@unimelb.edu.au>
2019-10-11 16:56:30 +1100

    Fingerprint authentication

--- app/src/main/java/com/team33/meetingmate/MainActivity.java
+++ app/src/main/java/com/team33/meetingmate/MainActivity.java
@@ -2,25 +2,58 @@ package com.team33.meetingmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
 import android.content.Intent;
+import android.hardware.fingerprint.FingerprintManager;
+import android.hardware.biometrics.BiometricManager;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.team33.meetingmate.ui.FingerprintHandler;
 import com.team33.meetingmate.ui.authentication.LoginActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+@TargetApi(11)
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
     private Button logoutBtn;
     private FirebaseAuth mAuth;
+    private KeyStore keyStore;
+    private static final String KEY_NAME = "EDMTDev";
+    private Cipher cipher;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        FingerprintManager fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+
 
         Button startNormalActivity = (Button) findViewById(R.id.button_activity);
         startNormalActivity.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +72,111 @@ public class MainActivity extends AppCompatActivity {
                 logout();
             }
         });
+
+        if (!fingerprintManager.isHardwareDetected())
+            Toast.makeText(this, "Fingerprint authentication permission not enable", Toast.LENGTH_SHORT).show();
+        else {
+            if (!fingerprintManager.hasEnrolledFingerprints())
+                Toast.makeText(this, "Register at least one fingerprint in Settings", Toast.LENGTH_SHORT).show();
+            else {
+                if (!keyguardManager.isKeyguardSecure())
+                    Toast.makeText(this, "Lock screen security not enabled in Settings", Toast.LENGTH_SHORT).show();
+                else
+                    genKey();
+
+                if (cipherInit()) {
+                    FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                    FingerprintHandler helper = new FingerprintHandler(this);
+                    //FingerprintHandler helper = new FingerprintHandler(this);
+                    helper.startAuthentication(fingerprintManager, cryptoObject);
+
+                }
+            }
+        }
     }
+
+    private boolean cipherInit() {
+
+        try {
+            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES+"/"+KeyProperties.BLOCK_MODE_CBC+"/"+KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keyStore.load(null);
+            SecretKey key = (SecretKey)keyStore.getKey(KEY_NAME,null);
+            cipher.init(Cipher.ENCRYPT_MODE,key);
+            return true;
+        } catch (IOException e1) {
+
+            e1.printStackTrace();
+            return false;
+        } catch (NoSuchAlgorithmException e1) {
+
+            e1.printStackTrace();
+            return false;
+        } catch (CertificateException e1) {
+
+            e1.printStackTrace();
+            return false;
+        } catch (UnrecoverableKeyException e1) {
+
+            e1.printStackTrace();
+            return false;
+        } catch (KeyStoreException e1) {
+
+            e1.printStackTrace();
+            return false;
+        } catch (InvalidKeyException e1) {
+
+            e1.printStackTrace();
+            return false;
+        }
+
+    }
+
+    private void genKey() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+        KeyGenerator keyGenerator = null;
+
+        try {
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES,"AndroidKeyStore");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keyStore.load(null);
+            keyGenerator.init(new KeyGenParameterSpec.Builder(KEY_NAME,KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT).setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7).build()
+            );
+            keyGenerator.generateKey();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+        catch (InvalidAlgorithmParameterException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
     private void logout(){
         mAuth.signOut();
